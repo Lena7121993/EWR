8381ab073e381f3619c6c59d5f5c0ea48cd7ac8f
Lena7121993 <64777379+Lena7121993@users.noreply.github.com>
2020-05-04 21:18:18 +0200

    Update aufgabe02.m

--- workspace/a02/aufgabe02.m
+++ workspace/a02/aufgabe02.m
@@ -4,7 +4,7 @@
 %impliziete Euler-Verfahren bestimmt. Als Test-Problem dient das
 %Anfangswertproblem nach dem Verhulst-Modell.
 
-%Autor: Lena,Hilpp ; Jan Frithjof Fleischhammer
+%Autor: Lena Hilpp ; Jan Frithjof Fleischhammer
 %Version: 03.05.2020
 
 %% Teilaufgabe a
@@ -14,22 +14,23 @@ clear;clc;close all;
 %Initialisierung der benoetigten Fenster
 openfigure(3,'init'); 
 
-%%Teilaufgabe b
+%% Teilaufgabe b
 %Modellparameter definieren
 d=2.9*10^(-2);
 a=2.941*10^(-3);
 c=1-d/(a*3.03);
 y0=3.03;
 t0=1960;
-%was ist t1?
-t1=2100;
+t1=2200;
+
+t=(1960:10:2200);
 
 %exakte Loesung
-yex=@(t) (d/a)./(1-c*exp(-d(t-t0)));
+yex=@(t) (d/a)./(1-c*exp(-d*(t-t0)));
 %Diff.Gl. Verhulst Modell
-verhulst=@(tt,yy) yy(d-a*yy);
+verhulst=@(tt,yy) yy*(d-a*yy);
 
-%%Teilaufgabe c
+%% Teilaufgabe c
 %Initialisierung der Variablen fuer Konvergenzanalyse
 
 %Anzahl an Rechnungen
@@ -38,20 +39,20 @@ n_cycles=6;
 dt=zeros(n_cycles,1);
 % Anzahl an Intervallen der Diskretisierung
 NN=zeros(n_cycles,1);
-% Fehler 
+% Fehler expl.Euler
 err=zeros(n_cycles,1);
-% benoetigte Rechenzeit
+% benoetigte Rechenzeit 
 ctm=zeros(n_cycles,1);
 
 %Laden der Standard Optionen
 opts=tb_thetaEuler();
 %Option fuer Verfahren einstellen 
 opts.theta=0;
-opts.theta=0.5;
-opts.theta=1;
+%opts.theta=0.5;
+%opts.theta=1;
 
-%%Teilaufgabe d
-fprintf('Die Rechnung ist im Gang')
+%% Teilaufgabe d
+fprintf('Die Rechnung beginnt');
 
 figure(1);
 clf;
@@ -65,44 +66,43 @@ for cycle=1:n_cycles,
     %Berechnung der Loesung
     [t,y]=tb_thetaEuler(verhulst,[t0,t1],y0,opts);
     %Zeitmessung beenden
-    toc;
     ctm(cycle)=max(toc,1e-8);
     hold on;
-    plot(t,y(t),'k-','linewidth',2);
+    plot(t,y,'k-','linewidth',2);
     plot(t,yex(t),'b:','linewidth',2);
-    xlabel('x');
-    ylabel('y');
-    legend('num.Lsg','exakte Lsg')
+    xlabel('Jahre');
+    %ylabel('Lösung?');
+    legend('num.Lsg','exakte Lsg','location','SouthEast');
     hold off;
     drawnow;
-    err(cycle)=norm(y-yex,inf);
+    err(cycle)=max(abs(y-yex(t)));
+    %err(cycle)=norm(y-yex(t),inf);
     dt(cycle)=tau;
-    %Anzahl unbekannte?
-    NN(cycle)= ;
-    pause;
+    NN(cycle)=ceil((t1-t0)/dt(cycle));
+    pause(3)
 end
+print('-f1','bild1','-dpng','-r100');
 
-%%Teilaufgabe e
+
+%% Teilaufgabe e
 %Konvergenzanalyse mittels eoctool
 figure(2);
 clf;
-%eoc mit TEX Datei
-[eoc,cst]=eoctool(N,ERR,2,1,{'expl. Euler','Crank-Nicolson','impli. Euler'},'eoc-vergleich.tex');
+%eoctool mit TEX Datei
+[eoc,cst]=eoctool(NN,err,2,1,{'expl. Euler','impli. Euler','Crank-Nicolson'},'eoc-tabelle.tex');
 
+%gemittelte Werte für eoc(experimentelle Konvergenzordnung) und
+%cst(experimentelle Konstante der Fehlerabschaetzung
 aveoc = mean(eoc(2:end,1));
 avcst = mean(cst(2:end,1));
 fprintf('Fehlerschaetzung: E = %5.2e * dt^{%5.2f}\n',avcst,-aveoc);
 
+print('-f2','bild2','-dpng','-r100');
 
-%%Teilaufgabe f
+
+%% Teilaufgabe f
 %statt Fehler Rechenzeiten uebergeben
 figure(3);
-[eoc,cst] = eoctool(NN,ctm);
+[eoc,cst] = eoctool(NN,ctm,2,1,{'Rechenzeit'},'eoc-Rechenzeiten.tex');
 
-
-    
-
-
-
-
-
+print('-f3','bild3','-dpng','-r100');
